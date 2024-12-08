e759162d3d9c02684e74d1cfdf8f5f67af451951
Peter Baintner <git@dreisicht.net>
2024-12-08 13:06:38 +0100

    Added preprocessor directive transition for FIXED_SEED

--- src/section15/AccountTest.cc
+++ src/section15/AccountTest.cc
@@ -8,25 +8,25 @@ TEST(SimpleTest, NoArgs) {
   Account acc{};
   EXPECT_EQ(acc.get_name(), "Unnamed Account");
   EXPECT_EQ(acc.get_balance(), 0.0);
-  EXPECT_EQ(acc.get_uid(), "P9");
+  EXPECT_EQ(acc.get_uid(), "MI");
 }
 TEST(SimpleTest, NameArg) {
   Account acc{"Larry", false};
   EXPECT_EQ(acc.get_name(), "Larry");
   EXPECT_EQ(acc.get_balance(), 0.0);
-  EXPECT_EQ(acc.get_uid(), "P9");
+  EXPECT_EQ(acc.get_uid(), "MI");
 }
 TEST(SimpleTest, NameArg2) {
   Account acc{"Moe", 2000};
   EXPECT_EQ(acc.get_name(), "Moe");
   EXPECT_EQ(acc.get_balance(), 2000.0);
-  EXPECT_EQ(acc.get_uid(), "P9");
+  EXPECT_EQ(acc.get_uid(), "MI");
 }
 TEST(SimpleTest, NameArg3) {
   Account acc{"Curly", 5000};
   EXPECT_EQ(acc.get_name(), "Curly");
   EXPECT_EQ(acc.get_balance(), 5000.0);
-  EXPECT_EQ(acc.get_uid(), "P9");
+  EXPECT_EQ(acc.get_uid(), "MI");
 }
 
 int main(int argc, char **argv) {
